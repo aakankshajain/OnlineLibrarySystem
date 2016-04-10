1bb940e77e0eaa8d68f808ae2c3ba27512a6656f
aakanksha jain <aakanksha.bwr@gmail.com>
2016-04-10 20:08:05 +0530

    Implementing searching functionality for user from where he can also be able to issue a available book.

--- installationScript/sqlScript.sql
+++ installationScript/sqlScript.sql
@@ -1,20 +1,33 @@
 create table userRegistration{
-	studentId varchar2(20)primary key;
-	emailId varchar2(20);
-	name varchar2(50);
-	phoneNumber integer;
-	password varchar2(50);
+	studentId varchar2(20)primary key,
+	emailId varchar2(20),
+	name varchar2(50),
+	phoneNumber integer,
+	password varchar2(50),
 	
 };
 
 create table bookRegistration{
-	bookNumber varchar2(20)primary key;
-	bookName varchar2(20);
-	author varchar2(50);
-	subject varchar2(50);
-	bookType char(1);
-	eBook BLOB;
+	bookNumber varchar2(20)primary key,
+	bookName varchar2(20),
+	author varchar2(50),
+	subject varchar2(50),
+	bookType char(1),
+	eBook BLOB,
 	
+};
+
+create table bookIssue{
+	SNo integer PRIMARY KEY,
+	bookNumber varchar2(20),
+	studentId varchar2(20),
+	issueDate DATE,
+	dueDate DATE,
+	avalability char(1);
+	FOREIGN KEY(bookNumber) REFERENCES bookRegistration(bookNumber),
+	FOREIGN KEY(studentId) REFERENCES userRegistration(studentId),
+	
+
 };
 
 commit;
